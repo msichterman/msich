db03c52c5339a2b39c77524fb954d4e205f69838
Matt Sichterman <msichterman1@gmail.com>
2022-12-12 21:08:30 -0500

    feat(snow): Made it snow

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ import { useEffect, useRef } from "react";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { trpc } from "@/utils/trpc";
+import Script from "next/script";
 
 function usePrevious(value: string) {
   const ref = useRef<string>();
@@ -27,23 +28,26 @@ const MyApp: AppType<{ session: Session | null }> = ({
   const previousPathname = usePrevious(router.pathname);
 
   return (
-    <SessionProvider session={session}>
-      <>
-        <div className="fixed inset-0 flex justify-center sm:px-8">
-          <div className="flex w-full max-w-7xl lg:px-8">
-            <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+    <>
+      <Script src="https://app.embed.im/snow.js" defer />
+      <SessionProvider session={session}>
+        <>
+          <div className="fixed inset-0 flex justify-center sm:px-8">
+            <div className="flex w-full max-w-7xl lg:px-8">
+              <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+            </div>
           </div>
-        </div>
-        <div className="relative">
-          <Header />
-          <main>
-            <Component previousPathname={previousPathname} {...pageProps} />
-            <Analytics />
-          </main>
-          <Footer />
-        </div>
-      </>
-    </SessionProvider>
+          <div className="relative">
+            <Header />
+            <main>
+              <Component previousPathname={previousPathname} {...pageProps} />
+              <Analytics />
+            </main>
+            <Footer />
+          </div>
+        </>
+      </SessionProvider>
+    </>
   );
 };
 
